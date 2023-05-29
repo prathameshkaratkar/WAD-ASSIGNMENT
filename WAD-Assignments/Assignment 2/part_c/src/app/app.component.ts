7773e27559f8dab5c705baf724cd77c8fbb5d0d1
platinum <117072460+prathameshkaratkar@users.noreply.github.com>
2023-05-29 07:24:38 +0530

    Update app.component.ts

--- WAD-Assignments/Assignment 2/part_c/src/app/app.component.ts
+++ WAD-Assignments/Assignment 2/part_c/src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'User Login Dashboard';
+  title = 'Registration Form';
+
+  displayname='';
+  displayaddress='';
+  displaycontact='';
+  displayemail='';
+
+  getValue(name:string, address:string, contact:string, email:string){
+    this.displayname=name;
+    this.displayaddress=address;
+    this.displaycontact=contact;
+    this.displayemail=email;
+  }
 }
